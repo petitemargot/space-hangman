70ea97efdf09eeeed997d338721fa4e208138b96
Gosia Dittmajer <dittmajergosia@gmail.com>
2020-10-05 14:09:55 +0200

    add(script): added game mechanism, updated styles, add typing effect, still wip at hangman

---
+++ scripts/main.js
@@ -0,0 +1,79 @@
+const starWarsWords = [`amidala`, `anakin skywalker`, `army`, `artoo-deetoo`, `aunt beru`, `bail organa`, `bobba fett`,
+    `buzz droid`, `chanceller`, `chewbacca`, `clone wars`, `coruscant`, `count dooku`, `dagobah`, `dantooine`,
+    `darksaber`, `darth maul`, `darth sidious`, `darth tyrannous`, `darth vader`, `death star`, `destroyer`, `droid`,
+    `ewoks`, `force`, `galaxy`, `grievous`, `han solo`, `hutt`, `jabba`, `jar jar binks`, `jawa`, `jedi`, `jedi academy`,
+    `knight`, `lando calrissian`, `lea organa`, `lightsaber`, `luke skywalker`, `mace windu`, `master`, `master yoda`,
+    `midi-chlorians`, `millenium falcon`, `mos eisley`, `obi-wan kenobi`, `padme`, `palpatine`, `podracing`, `qui-gon jin`,
+    `rebel alliance`, `republic`, `sandpeople`, `see-threepio`, `senator`, `separatist`, `ship`, `sith`, `tatooine`];
+
+const letters = `abcdefghijklmnopqrstuvwxyz`;
+
+let answer = ``;
+let maxWrong = 7;
+let mistakes = 0;
+let guessed = [];
+let wordStatus = null;
+
+function randomWord() {
+    answer = starWarsWords[Math.floor(Math.random() * starWarsWords.length)];
+}
+
+function generateAlphabet() {
+    let alphabet = letters.split(``).map(letter =>
+        `<button
+        class="letter button" id="` + letter + `"
+        onclick="handleGuess('` + letter + `')">
+        ` + letter + `
+         </button>`
+    ).join('');
+
+    document.getElementById('keyboard').innerHTML = alphabet;
+}
+
+function handleGuess(letter) {
+
+    if (!letter) return false;
+    if (guessed.indexOf(letter) === -1) guessed.push(letter)
+    document.getElementById(letter).setAttribute('disabled', 'true');
+
+    if (answer.indexOf(letter) >= 0) guessedWord();
+    else if (answer.indexOf(letter) === -1) {
+        mistakes++;
+        updateMistakes();
+    }
+    checkGameStatus();
+}
+
+function guessedWord() {
+    wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
+    document.getElementById('word-spotlight').innerHTML = wordStatus;
+}
+
+
+function checkGameStatus() {
+
+    if (wordStatus === answer && mistakes !== maxWrong) document.getElementById('keyboard').innerHTML = `You Won!`;
+    else if (wordStatus !== answer && mistakes === maxWrong) {
+        document.getElementById('keyboard').innerHTML = `You Lost!`;
+        document.getElementById('word-spotlight').innerHTML = 'The answer was: ' + answer;
+    }
+}
+
+function updateMistakes() {
+    document.getElementById('mistakes').innerHTML = mistakes;
+}
+
+function reset() {
+    mistakes = 0;
+    guessed = [];
+    randomWord();
+    guessedWord();
+    updateMistakes();
+    generateAlphabet();
+}
+
+document.getElementById('maxWrong').innerHTML = `${maxWrong}`;
+
+randomWord();
+generateAlphabet();
+guessedWord();
